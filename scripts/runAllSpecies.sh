bc0f458515dc876488d478b6602b9fc7a43e0565
nunogit <nunomsilvanunes@gmail.com>
2019-07-01 20:27:42 +0200

    species batch

---
+++ scripts/runAllSpecies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for filename in resource/*.config; do
+    for ((i=0; i<=3; i++)); do
+	java -jar target/BioMart2BridgeDb-jar-with-dependencies.jar resource/$filename ./bridgefiles
+        #./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
+    done
+done
