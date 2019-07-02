55baaf3fb2d6b429d13197bfbf2f5286111c9583
nunogit <nunomsilvanunes@gmail.com>
2019-07-02 17:18:50 +0200

    minor fixes to runallspecies

--- scripts/runAllSpecies.sh
+++ scripts/runAllSpecies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 for filename in resource/*.config; do
-    for ((i=0; i<=3; i++)); do
-	java -jar target/BioMart2BridgeDb-jar-with-dependencies.jar $filename ./bridgefiles
+	java -jar target/BioMart2BridgeDb-jar-with-dependencies.jar $filename ./bridgefiles/
         #./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
-    done
 done
